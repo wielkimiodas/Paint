09bfe14cac4b929e76f28dc489167848b4b35011
miodas <wielkimiodas@gmail.com>
2013-06-26 15:22:00 +0200

    lab example

---
+++ lab example/ConsoleApplication2/ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public interface Plugin2 : DLL.Plugin
+    {
+        string Message2();
+    }
+
+    public class Class1 : DLL.Plugin
+    {
+        public string Message()
+        {
+            return "Class1";
+        }
+    }
+
+    public class Class2 : DLL.Plugin
+    {
+        public string Message()
+        {
+            return "Class2";
+        }
+    }
+
+    abstract public class Class3 : Plugin2
+    {
+        abstract public string Message();
+
+        public string Message2()
+        {
+            return "Class3";
+        }
+    }
+
+    public class Class4
+    {
+
+    }
+}
---
+++ lab example/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = Assembly.LoadFrom(@"F:\ConsoleApplication2\ClassLibrary1\bin\Debug\ClassLibrary1.dll");
+            List<Type> types = new List<Type>();
+            Type[] T = a.GetTypes();
+            foreach(Type t in T)
+            {
+                if (!t.IsPublic || !t.IsClass || t.IsAbstract)
+                    continue;
+
+                if (typeof(DLL.Plugin).IsAssignableFrom(t))
+                    types.Add(t);
+            }
+
+            foreach (Type t in types)
+            {
+                DLL.Plugin p = (DLL.Plugin)Activator.CreateInstance(t);
+                Console.WriteLine(p.Message());
+            }
+        }
+    }
+}
